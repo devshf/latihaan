392c0f4f4fb80b58cd232402f233d3f01894cfe2
deva shofa al fathin <devasa@upi.edu>
2023-03-12 22:56:52 +0700

    Co-authored-by: Farah Dwi Ameliani <Farahdwi21@users.noreply.github.com>

---
+++ android/app/src/main/kotlin/com/example/pushagain/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pushagain
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); //test yuu
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello'),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
